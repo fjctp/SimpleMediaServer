af92b1a8838fe673373aeeeea89371901ffc1f41
snchan <snchan20@yahoo.com>
2015-03-04 20:38:00 -0800

    update browser's layout: folder in one column, videos in another column; update the title for root to 'home'

--- handlers/browserHandler.js
+++ handlers/browserHandler.js
@@ -14,33 +14,38 @@ function writeResponse (parsed_request, response) {
     fs.readdir(folder_path, function (err, files) {
         if (err)
             throw err;
+        
+        // Get a list of videos and folders
         var files_list = {};
+        var folders_list = {};
         files.forEach(function (file) {
-            // for each file in the folder if it is a directory or video
-            // add it to the list, { file: url }
             var stats = fs.statSync(path.join(folder_path, file));
             if (err)
                 throw err;
             var get_path = path.join(parsed_request.get_path, file)
             if (stats.isDirectory())
                 // it is a directory, open browser
-                files_list[file] = 'browser?get=' + get_path;
+                folders_list[file] = 'browser?get=' + get_path;
             else if (path.extname(file) in include.video_type)
                 // it is a video, open player
                 files_list[file] = 'player?get=' + get_path;
         });
+        
         var html_path = path.join(
             include.path.ROOT,
             include.path.JADE_FOLDER,
             'browser.jade'
-        );
+        )
         var jade_options = {
             pageData: {
-                'title': parsed_request.get_path,
+                'title': parsed_request.get_path == '' ?
+                    'Home' : parsed_request.get_path, // default to Home
                 'previous_page': '',
                 'files': files_list,
+                'folders': folders_list,
             }
         };
+        
         // render jade template, and get the html content
         var html_content = jade.renderFile(html_path, jade_options)
         // write the html_content to the response
